e2df27d0e1c965dcb491173507b0e5cdf17cb37b
sune871 <dzl15688721817@gmail.com>
2025-09-11 04:15:13 +0800

    Delete src directory

--- src/types/cp_swap.rs
+++
@@ -1,7 +0,0 @@
-use borsh::{BorshSerialize, BorshDeserialize};
-
-#[derive(BorshSerialize, BorshDeserialize, Debug, Clone)]
-pub struct SwapBaseInput {
-    pub amount_in: u64,
-    pub min_amount_out: u64,
-} 
